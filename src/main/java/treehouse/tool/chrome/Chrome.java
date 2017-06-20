a42ffa60c5410a4a639373b12a3f2a8fc36b1a3b
Aaron Brayall <abrayall@yahoo.com>
2017-06-20 13:48:21 -0400

    updating so finding/launchnig chrome works on macos

--- src/main/java/treehouse/tool/chrome/Chrome.java
+++ src/main/java/treehouse/tool/chrome/Chrome.java
@@ -1,6 +1,6 @@
 package treehouse.tool.chrome;
 
-import static javax.lang.System.now;
+import static javax.lang.System.*;
 import static javax.util.List.list;
 
 import java.net.URL;
@@ -25,18 +25,18 @@ public class Chrome extends Launcher {
 	protected Process process;
 	protected SessionFactory factory = new SessionFactory();
 	protected ChromeWatcher watcher;
-	
+
 	public static String DEV_TOOLS_ENABLED = "--auto-open-devtools-for-tabs";
-	
+
 	public Chrome launch(URL url, String... options) throws Exception {
 		this.process = this.execute(list("--disable-web-security", "--test-type", "--no-sandbox").append(options).append(url.toString()).toArray(new String[0]));
 		return this;
 	}
-	
+
 	public Process execute(String... arguments) throws Exception {
         String chromePath = findChrome();
         Path remoteProfileData = Paths.get(System.getProperty("java.io.tmpdir")).resolve("remote-profile");
-        
+
         List<String> list = new ArrayList<>();
         list.add(chromePath);
         list.add(String.format("--remote-debugging-port=%d", factory.getPort()));
@@ -52,69 +52,73 @@ public class Chrome extends Launcher {
         list.add("--disable-popup-blocking");
         if (arguments != null)
             list.addAll(Arrays.asList(arguments));
-       
+
         Process process = Runtime.getRuntime().exec(list.toArray(new String[0]));
         process.getOutputStream().close();
         process.getInputStream().close();
     	return process;
     }
-	
+
 	public List<SessionInfo> sessions() {
 		return this.process != null ? this.factory.list() : list();
 	}
-	
+
 	public Session get(String url) {
 		for (SessionInfo session : this.sessions()) {
 			if (session.getUrl().equals(url))
 			    return this.factory.connect(session.getId());
 		}
-		
+
 		return null;
 	}
-	
+
 	public Session open(String url) {
 		return this.factory.create().navigate(url);
 	}
-	
+
 	public Chrome close() {
 		if (this.factory != null) {
 			Try.attempt(() -> this.factory.close());
 			this.factory = null;
 		}
-		
+
 		if (this.process != null) {
 			this.process.destroyForcibly();
 			this.process = null;
 		}
-		
+
 		return this;
 	}
-	
+
 	public boolean closed() {
 		return this.process == null || this.process.isAlive() == false;
 	}
-	
+
 	public Chrome closed(Consumer<Long> handler) {
 		this.watcher = new ChromeWatcher(this, handler).watch();
 		return this;
 	}
-	
+
 	public Chrome cancel() {
 		if (this.watcher != null) {
 			this.watcher.halt();
 			while (this.watcher.isAlive() == true)
 				Time.sleep(100, TimeUnit.MILLISECONDS);
 		}
-		
+
 		return this;
 	}
-	
+
+	public String findChrome() {
+		return isMac() ? "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" : super.findChrome();
+	}
+
 	private class ChromeWatcher extends Thread {
-		
+
 		private Chrome chrome;
 		private Consumer<Long> handler;
 		private boolean running = false;
-		
+
 		public ChromeWatcher(Chrome chrome, Consumer<Long> handler) {
 			this.chrome = chrome;
 			this.handler = handler;
@@ -124,12 +128,12 @@ public class Chrome extends Launcher {
 			this.start();
 			return this;
 		}
-		
+
 		public void run() {
 			this.running = true;
 			while (this.running == true && this.chrome.closed() == false)
 				Time.sleep(100, TimeUnit.MILLISECONDS);
-				
+
 			if (this.running == true && this.handler != null) this.handler.accept(now());
 		}
 
@@ -137,28 +141,4 @@ public class Chrome extends Launcher {
 			this.running = false;
 		}
 	}
-	
-	public static void main(String[] arguments) throws Exception {
-	//	Browser browser = new Browser().launch("http://localhost:8015/index.html", Browser.DEV_TOOLS_ENABLED);
-		//Thread.sleep(3000);
-		//SessionFactory browser = , "http://localhost:8015/index.html");
-		//SessionFactory factory = new SessionFactory();
-		
-	   // Thread.sleep(5000);
-   		//Session window = browser.get("http://localhost:8015/index.html");
-        //window.waitDocumentReady();
-        //window.activate();
-        //window.getCommand().getEmulation().setVisibleSize(375, 667);
-	            
-      //  browser.close();
-        //try (Session session = factory.create()) {
-        //    session.navigate("http://localhost:8015/index.html");
-        //    session.waitDocumentReady();
-            // activate the tab/session before capturing the screenshot
-        //    session.activate();
-        //   Streams.write(session.captureScreenshot(), new FileOutputStream(file.toFile()));
-            
-        //    Thread.sleep(10000000);
-        //}
-    }
 }
