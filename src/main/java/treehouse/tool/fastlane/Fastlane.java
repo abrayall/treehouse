60d8001e9786c26c21367eabd5ede901bb2d37f9
Aaron Brayall <abrayall@yahoo.com>
2017-06-23 08:45:00 -0400

    updating to support publishing to apple itunes store

--- src/main/java/treehouse/tool/fastlane/Fastlane.java
+++ src/main/java/treehouse/tool/fastlane/Fastlane.java
@@ -1,6 +1,7 @@
 package treehouse.tool.fastlane;
 
 import static javax.util.List.list;
+import static javax.util.Map.*;
 
 import javax.io.File;
 import javax.lang.Try;
@@ -10,19 +11,34 @@ import treehouse.App;
 import treehouse.tool.Tool;
 
 public class Fastlane extends Tool {
-	
+
 	protected String name() {
 		return "fastlane";
 	}
-	
+
 	public String getVersion() throws Exception {
 		return Try.attempt(() -> list(execute("--version").split("\n")).last().split(" ")[1], "0.0.0");
 	}
-	
+
 	public Process supply(App app, String track, File artifact, File key, List<String> extra) throws Exception {
 		return this.process(new File("."), arguments(extra, "supply", "--apk", artifact.toString(), "--track", track, "--package_name", app.getId(), "--json_key", key.toString())).start();
 	}
-	
+
+	public Process deliver(App app, File artifact, String username, String password, List<String> extra) throws Exception {
+		return this.builder(
+			map(entry("FASTLANE_DONT_STORE_PASSWORD", "1"), entry("FASTLANE_PASSWORD", password)),
+			new File("."),
+			arguments(
+				extra,
+				"deliver",
+				"--ipa", artifact.toString(),
+				"--app_identifier", app.getId(),
+				"--force",
+				"--username", username
+			)
+		).start();
+	}
+
 	protected String[] arguments(List<String> extra, String... parameters) {
 		List<String> arguments = list(parameters);
 		arguments.addAll(extra);
--- src/main/java/treehouse/tool/fastlane/FastlanePublisher.java
+++ src/main/java/treehouse/tool/fastlane/FastlanePublisher.java
@@ -1,8 +1,7 @@
 package treehouse.tool.fastlane;
 
-import static javax.util.List.list;
-import static javax.util.Map.entry;
-import static javax.util.Map.map;
+import static javax.util.Map.* ;
+import static javax.util.List.*;
 
 import javax.io.File;
 import javax.lang.Process;
@@ -14,30 +13,46 @@ import treehouse.Engine;
 
 public abstract class FastlanePublisher {
 
-	public static FastlanePublisher publisher(String platform, Engine engine) {
-		if ("android".equals(platform))
-			return new FastlanePlayPublisher(engine);
+	protected Engine engine;
 
-		return null;
+	protected boolean result = false;
+	protected String reason = "";
+
+	public FastlanePublisher(Engine engine) {
+		this.engine = engine;
 	}
 
 	public abstract Future<Boolean> publish(App app, File artifact, String track, Map<String, String> options) throws Exception;
+
+	public Future<Boolean> publish(App app, File artifact, String track) throws Exception {
+		return this.publish(app, artifact, track, map());
+	}
+
+	public void finish(Integer code, Future<Boolean> job) {
+		if (this.result)
+			job.complete(true);
+		else
+			job.completeExceptionally(new Exception(this.reason));
+	}
+
+	public static FastlanePublisher publisher(String platform, Engine engine) {
+		if ("android".equals(platform))
+			return new FastlanePlayPublisher(engine);
+		else if ("ios".equals(platform))
+			return new FastlaneApplePublisher(engine);
+
+		return null;
+	}
 
 	public static class FastlanePlayPublisher extends FastlanePublisher {
 
 		protected Engine engine;
 
 		protected boolean result = false;
-		protected String reason = "";
+		protected String reason = "Unknown error";
 
 		public FastlanePlayPublisher(Engine engine) {
-			this.engine = engine;
-		}
-
-		public Future<Boolean> publish(App app, File artifact, String track) throws Exception {
-			return this.publish(app, artifact, track, map(
-				entry("verbose", "true")
-			));
+			super(engine);
 		}
 
 		public Future<Boolean> publish(App app, File artifact, String track, Map<String, String> options) throws Exception {
@@ -52,7 +67,7 @@ public abstract class FastlanePublisher {
 			//if (verbose)
 			//	System.out.println("  [fastlane] " + line);
 
-			if (line.contains("Successfully finished the upload to Google Play"))
+			if (line.contains("Successfully finished the upload"))
 				this.result = true;
 
 			if (line.contains("Google Api Error")) {
@@ -60,12 +75,38 @@ public abstract class FastlanePublisher {
 				this.reason = line.split(":")[2];
 			}
 		}
+	}
 
-		public void finish(Integer code, Future<Boolean> job) {
-			if (this.result)
-				job.complete(true);
-			else
-				job.completeExceptionally(new Exception(this.reason));
+	public static class FastlaneApplePublisher extends FastlanePublisher {
+
+		protected boolean error = false;
+
+		public FastlaneApplePublisher(Engine engine) {
+			super(engine);
+		}
+
+		public Future<Boolean> publish(App app, File artifact, String track, Map<String, String> options) throws Exception {
+			return new Process(this.engine.fastlane().deliver(app, artifact, options.get("username", "default@apple.com"), options.get("password", "default"), list(
+				"--skip_metadata",
+				"--skip_screenshots"
+			))).future(Boolean.class).onOutput((line, job) -> this.handle(line, job, Boolean.parseBoolean(options.get("verbose", "false")))).onTerminate(this::finish);
+		}
+
+		public void handle(String line, Future<Boolean> job, Boolean verbose) {
+			if (verbose)
+				System.out.println("  [fastlane] " + line);
+
+			if (line.contains("Successfully uploaded package"))
+				this.result = true;
+
+			if (this.error == true && line.startsWith("\t"))
+				this.reason = line.trim();
+
+			if (line.contains("[!]")) {
+				this.result = false;
+				this.error = true;
+				this.reason = line.split("\\[\\!\\]")[1].replace("Check out the error above", "");
+			}
 		}
 	}
 }
